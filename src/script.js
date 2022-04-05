cd5721e5f020223d71c20c73824d9c6776ee9903
Razvan <razvanbugoi@gmail.com>
2022-04-05 05:50:27 +0100

    working on the JavaScript logic

--- src/script.js
+++ src/script.js
@@ -1,10 +1,23 @@
-const Gameboard = () => {
-  let gameboard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-  ]; 
+const Game = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
   const playerX = "X";
   const playerO = "O";
+  const cellElemenets = Array.from(document.querySelectorAll(".cell"));
 
-}
+  cellElemenets.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true })
+  })
+
+  function handleClick(e) {
+    const cell = e.target
+    const cellIndex = cell.dataset.index;
+  
+    gameboard[cellIndex] = "X";
+    cell.textContent = "X";
+    
+  }
+
+  return {gameboard, playerO, playerX}
+})();
+
+
